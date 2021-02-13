9448c5061ff3b4e213ca6db77aada5e8035f2ba2
Emiliyan <emiliyan.kadiyski@shkolo.com>
2021-02-13 16:05:06 +0200

    Add links styling on static pages

--- resources/views/static_pages/standard.blade.php
+++ resources/views/static_pages/standard.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
     	<h1 class="col-12 text-center py-8">{{ $pageContent['title'] }}</h1>
-    	<div class="col-12 py-2">
+    	<div class="col-12 py-2" id="static-page-content">
     		{!! $pageContent['content'] !!}
     	</div>
     </div>
